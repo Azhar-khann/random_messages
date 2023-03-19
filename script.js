67639964321a0fab1d1c2c982bd851b64e01ecd8
Azharuddinn <Azharuddinkhann1@gmail.com>
2023-03-19 17:28:49 +0500

    function added to produce random inspirational message

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const msg_1 = ['are capable.', 'matter.','can do it', 'are not alone','are enough','have potential','are unique',
     'are loved','make a difference','are worthy']
+
+function produce_random_message(msg_array){
+    random_num = Math.floor(Math.random())*10
+    return 'You' + msg_array[random_num]
+}
+
